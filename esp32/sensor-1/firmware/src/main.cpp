4d7c3f575a73112ecd8b2e065d52defe7cc6a6ff
Charles Stolz <rtikes@protonmail.com>
2025-02-12 20:07:28 -1000

    adding source tested on three devices

---
+++ esp32/sensor-1/firmware/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "sensor.h"
+
+#define NODE_NAME "Sensor-1"
+
+void setup() {
+    Serial.begin(115200);
+    Serial.println();
+    Serial.print("Booting Node: ");
+    Serial.println(NODE_NAME);
+
+    initSensor();
+}
+
+void loop() {
+    readSensor();
+    delay(1000);
+}
---
+++ esp32/sensor-2/firmware/src/sensor.h
@@ -0,0 +1,9 @@
+#ifndef SENSOR_H
+#define SENSOR_H
+
+#include <Arduino.h> 
+
+void initSensor();
+void readSensor();
+
+#endif
